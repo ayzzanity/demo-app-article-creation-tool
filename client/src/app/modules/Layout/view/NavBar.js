71d7d8f02317b85dacec1a4d745784d0e7a35702
Ayz Abdulwahid <ayz.abdulwahid@gmail.com>
2021-08-24 13:53:11 +0800

    added links to nav bar

--- client/src/app/modules/Layout/view/NavBar.js
+++ client/src/app/modules/Layout/view/NavBar.js
@@ -1,4 +1,7 @@
 import { Layout, Menu } from 'antd';
+import { Link } from 'react-router-dom';
+import { Routes as CoreRoutes } from '@core_routing/';
+import { Routes as AppRoutes } from '@app_routing/';
 
 const NavBar = ({ children }) => {
   const { Header, Content } = Layout;
@@ -6,9 +9,15 @@ const NavBar = ({ children }) => {
     <Layout style={{ height: '100%', overflow: 'auto' }}>
       <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key={1}>HOME</Menu.Item>
-          <Menu.Item key={2}>VIEW ARTICLES</Menu.Item>
-          <Menu.Item key={3}>MANAGE</Menu.Item>
+          <Menu.Item key={1}>
+            <Link to={CoreRoutes.HOME_ROUTE}>HOME</Link>
+          </Menu.Item>
+          <Menu.Item key={2}>
+            <Link to={AppRoutes.ARTICLES_ROUTE}>VIEW ARTICLES</Link>
+          </Menu.Item>
+          <Menu.Item key={3}>
+            <Link to={AppRoutes.ARTICLE_MANAGEMENT_ROUTE}>MANAGE</Link>
+          </Menu.Item>
         </Menu>
       </Header>
       <Content style={{ paddingTop: '50px' }}>{children}</Content>
